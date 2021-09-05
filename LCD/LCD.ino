2fdd08a2be1a77a660a9e2b724ecf3e35b046584
Hector Villasano <hectorstevenvillasano@gmail.com>
2021-09-04 20:43:25 -0700

    scroll view

--- LCD/LCD.ino
+++ LCD/LCD.ino
@@ -8,27 +8,29 @@ int d6 = 11;
 int d7 = 12;
 
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-  
+
+String displayText = "Follow @stoiczfpv on IG!";
+
 void setup() {
   lcd.begin(16, 2);
+  Serial.begin(9600);
 }
 
-bool completed = false;
 void loop() {
-  if (completed == false) {
-    for (int i = 0; i < 1234; i++) {
-      lcd.clear();
-      lcd.setCursor(random(1,15),0);
-      lcd.print(random(1,1000));
-      lcd.setCursor(random(1,15),1);
-      lcd.print(random(1,300));
-      delay(200);
-    }
-    
-    completed = true;
-    lcd.clear();
+  autoScroll(displayText);
+}
+
+void autoScroll(String text) {
+  lcd.setCursor(16,0);
+  lcd.autoscroll();
+  
+  for (int i = 0; i < displayText.length(); i++) {
+    char c = displayText[i];
+    Serial.println(c);
+    lcd.print(c);
+    delay(300);
   }
   
-  lcd.setCursor(5,1);
-  lcd.print("Complete...");
+//  lcd.noAutoscroll();
+//  lcd.clear();
 }
