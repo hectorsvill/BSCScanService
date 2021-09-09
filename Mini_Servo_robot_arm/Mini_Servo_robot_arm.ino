2f6fbb5439a117e8d81a01a921b6212a49fb0501
Hector Villasano <hectorstevenvillasano@gmail.com>
2021-09-08 21:00:23 -0700

    goToZero

--- Mini_Servo_robot_arm/Mini_Servo_robot_arm.ino
+++ Mini_Servo_robot_arm/Mini_Servo_robot_arm.ino
@@ -1,38 +1,68 @@
+/*
+ * 
+ * Components:
+ *  - 5 x SG90 Servo
+ *  - PCA9685 16 Channel 12 Bit PWM Servo Motor Driver
+ * 
+ */
 
 #include <Servo.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
 
-int delayTime = 50;
+
+#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+#define SERVOMIN  110
+#define SERVOMAX  510
+#define SERVOCOUNT 5
+Servo servo1;
+Adafruit_PWMServoDriver pwmServoDriver = Adafruit_PWMServoDriver();
+
+int delayTime = 500;
 int servoPin = 9;
 int servoPosition=0;
-
-int servoMaxPosition = 60;
-
+int servoMaxPosition = 180;
 int servo1MaxPosition = 170;
 int servo2MaxPosition = 140;
 int servo3MaxPosition = 130;
 
-Servo servo1;
+// our servo # counter
+uint8_t servonum = 0;
 
 void setup() {
   Serial.begin(9600);
   servo1.attach(servoPin);
+//
+//  pwmServoDriver.begin();
+//  pwmServoDriver.setOscillatorFrequency(27000000);
+//  pwmServoDriver.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
+  delay(10);
 }
 
 void loop() {
-//  servo1.write(0);
-  int maxPosition = servoMaxPosition;
+  goToZero(servo1);
+  servo1.write(servoMaxPosition);
+  delay(delayTime);
   
-  for (int i = 0; i <  maxPosition; i+=1) {
-    servo1.write(i);
-    Serial.println(i);
-    delay(delayTime);
-  }
+//  Serial.println(servonum);
+//  pwmServoDriver.setPWM(0, 0, SERVOMIN + random(10, 30));
+//  delay(1000);
 
-  for (int i = maxPosition; i >= 0; i-=1) {
-    servo1.write(i);
-    Serial.println(i);
-    delay(delayTime);
-  }
+//  int maxPosition = servoMaxPosition;
+//  
+//  for (int i = 0; i <  maxPosition; i+=1) {
+////    servo1.write(i);
+//    pwmServoDriver.setPWM(0, 0, angleToPulse(i));
+//    Serial.println(i);
+//    delay(delayTime);
+//  }
+//
+//  for (int i = maxPosition; i >= 0; i-=1) {
+////    servo1.write(i);
+//    pwmServoDriver.setPWM(0, 0, angleToPulse(i));
+//    Serial.println(i);
+//    delay(delayTime);
+//  }
 }
 
 void randomPosition() {
@@ -48,4 +78,27 @@ void checkServoRadius() {
     servoPosition += 10;
     delay(delayTime);
   } 
+}
+
+void goToZero(Servo servo) {
+  
+  
+  while (servo.read() > 0) {
+    int pinPosition = servo.read();
+    pinPosition -= 5;
+    servo.write(pinPosition);
+    String str = "goToZero at position: " + String(pinPosition);
+    Serial.println(str);
+    delay(100);
+    if (pinPosition < 0) {
+      pinPosition = 0;
+    }
+  }
+}
+
+int angleToPulse(int angle) {
+  int pulse = map(angle, 0, 180,SERVOMIN, SERVOMAX);
+  String infoStr = "angle" + String(angle) + "pulse" + String(pulse);
+  Serial.println(infoStr);
+  return pulse; 
 }
